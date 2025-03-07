8df9bceae117fc62676ab462fd9cc6c7aef5dc3f
Diego Gallo <ingreydiru@gmail.com>
2025-03-07 14:38:52 -0300

    ds
    sdd

--- components/product/ProductOptions.tsx
+++ components/product/ProductOptions.tsx
@@ -1,23 +1,21 @@
 import type { Product } from "lib/shopify/types";
 
 export function getProductOptions(products: Product[]) {
-	const options = {
-		Tejido: new Set<string>(),
-		"Sexo objetivo": new Set<string>(),
+	const options: { Tejido: string[]; "Sexo objetivo": string[] } = {
+		Tejido: [],
+		"Sexo objetivo": [],
 	};
 
 	for (const product of products) {
 		for (const option of product.options) {
 			if (option.name === "Tejido" || option.name === "Sexo objetivo") {
-				for (const value of option.values) {
-					options[option.name].add(value);
-				}
+				options[option.name].push(...option.values);
 			}
 		}
 	}
 
 	return [
-		{ name: "Tejido", values: Array.from(options.Tejido) },
-		{ name: "Sexo objetivo", values: Array.from(options["Sexo objetivo"]) },
+		{ name: "Tejido", values: [...new Set(options.Tejido)] },
+		{ name: "Sexo objetivo", values: [...new Set(options["Sexo objetivo"])] },
 	];
-} 
+}
