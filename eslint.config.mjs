f683a7dbc2d0a78c3d0517682f9a7ea3ef354dbd
Diego Gallo <ingreydiru@gmail.com>
2025-03-07 05:13:19 -0300

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,7 @@ const eslintConfig = [
       '@typescript-eslint/no-explicit-any': 'off',
       'prefer-const': 'off',
       'react-hooks/exhaustive-deps': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
     },
   },
 ];
