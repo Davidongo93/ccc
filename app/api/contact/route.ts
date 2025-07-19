923375a0f470d704df8d874599f58b61eac98092
David Orlando Miranda Roa <cto@colombiancannabiscenter.com>
2025-07-19 11:53:29 -0500

    fix production mail

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,4 +1,4 @@
-import nodemailer from "nodemailer";
+import nodemailer from 'nodemailer';
 
 // Define la interfaz para los datos del formulario
 interface ContactFormData {
@@ -15,41 +15,36 @@ export async function POST(request: Request) {
 
     // Validación de los campos
     if (!name || !email || !message) {
-      return new Response(
-        JSON.stringify({ message: "Todos los campos son obligatorios" }),
-        { status: 400 }
-      );
+      return new Response(JSON.stringify({ message: 'Todos los campos son obligatorios' }), {
+        status: 400
+      });
     }
 
     // Configuración de Nodemailer
     const transporter = nodemailer.createTransport({
-      service: "Gmail", 
+      service: 'Gmail',
       auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
+        pass: process.env.EMAIL_PASS
+      }
     });
 
     const mailOptions = {
       from: email,
-      to: "analiagiachini@gmail.com", 
-      subject: `Nuevo mensaje de ${name}`,
-      text: `Nombre: ${name}\nEmail: ${email}\nMensaje: ${message}`,
+      to: 'cto@colombiancannabiscenter.com',
+      subject: `Nuevo mensaje de website ccc. de ${name}`,
+      text: `Nombre: ${name}\nEmail: ${email}\nMensaje: ${message}`
     };
 
     // Enviamos el correo
     await transporter.sendMail(mailOptions);
 
     // Respondemos con éxito
-    return new Response(
-      JSON.stringify({ message: "Mensaje enviado correctamente" }),
-      { status: 200 }
-    );
+    return new Response(JSON.stringify({ message: 'Mensaje enviado correctamente' }), {
+      status: 200
+    });
   } catch (error) {
-    console.error("Error enviando el email:", error);
-    return new Response(
-      JSON.stringify({ message: "Error al enviar el mensaje" }),
-      { status: 500 }
-    );
+    console.error('Error enviando el email:', error);
+    return new Response(JSON.stringify({ message: 'Error al enviar el mensaje' }), { status: 500 });
   }
 }
